ada185300957b31e6a71ed64ecb41b23eb236c7b
mimarchant <mimarchtt@gmail.com>
2024-09-08 14:26:56 -0300

    update modo no headless

--- index.js
+++ index.js
@@ -36,7 +36,7 @@ function sendEmail(subject, htmlContent) {
 // Función para hacer scraping
 async function checkLaptops() {
   const browser = await puppeteer.launch({
-    headless: true, // Cambia a false para modo no headless
+    headless: false, // Cambia a false para modo no headless
     args: [
       "--no-sandbox",
       "--disable-setuid-sandbox",
